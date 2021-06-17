fac6a2df9ab02dc63d01cb0560630cdfdbc2f903
MG_formation <marc.garce@free.fr>
2021-06-17 12:31:44 +0200

    commit 2eme jour midi

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Film;
+use App\Repository\FilmArrayRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\FilmRepository;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/film", name="film_")
@@ -27,7 +34,7 @@ class FilmController extends AbstractController
      */
     public function list(): Response
     {
-            $filRepo= new FilmRepository();
+            $filRepo= new FilmArrayRepository();
         return $this->render('film/list.html.twig', [
                 "films" => $filRepo->getAll()
             ]
@@ -39,7 +46,7 @@ class FilmController extends AbstractController
      */
     public function listJson(): Response
     {
-        $filRepo= new FilmRepository();
+        $filRepo= new FilmArrayRepository();
         return new JsonResponse($filRepo->getAll());
         
     }
@@ -49,10 +56,67 @@ class FilmController extends AbstractController
      */
     public function getFilm($idFilm): Response
     {
-        $filRepo= new FilmRepository();
+        $filRepo= new FilmArrayRepository();
         $fiche = $filRepo->get($idFilm);
         return $this->render('film/fiche.html.twig', [
             'laFiche' => $fiche
         ]);
+    }
+
+     /**
+     * @Route("/add", name="add")
+     * 
+     */
+    public function add(EntityManagerInterface $em): Response
+    {
+        $film = new Film([ "titre" => "titre from sqlite1"]);
+        $film->setGenre("null comme film");
+        $film->setSynopsis("smfsgfhsdlkhdslkghdslkghdslkds");
+
+
+        //utilisation de l'entityMangar pour les requetesde bases
+        $em->persist($film);
+        $em->flush();
+
+    // redirection vers la page des lists
+        return $this->redirectToRoute('film_list');
+    }
+
+     /**
+     * @Route("/addForm", name="addForm")
+     * 
+     */
+    public function addForm(Request $request , EntityManagerInterface $em): Response
+    {
+        $film = new Film();
+
+
+        $formAddFilm = $this->createFormBuilder($film)
+        ->add('titre', TextType::class)
+        ->add('synopsis', TextareaType::class)
+        ->add('genre', TextType::class)
+        ->add('poster', TextType::class)
+        ->add('Save', SubmitType::class, ['label' => "Enregister le film"] )->getForm();
+
+
+        // prise en compte de la reponse
+        $formAddFilm->handleRequest($request);
+
+        if ($formAddFilm->isSubmitted()) {
+            $film=$formAddFilm->getData();
+            //utilisation de l'entityMangar pour les requetesde bases
+        $em->persist($film);
+        $em->flush();
+
+    // redirection vers la page des lists
+        return $this->redirectToRoute('film_list');
+        }
+        else {
+
+        return $this->render('film/ficheAdd.html.twig', [
+            'formAddFilm' => $formAddFilm->createView()
+        ]);
+    }
+
     }
 }
---
+++ src/Entity/Film.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FilmRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FilmRepository::class)
+ */
+class Film
+{
+
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $synopsis;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $genre;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $poster;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $censure;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getSynopsis(): ?string
+    {
+        return $this->synopsis;
+    }
+
+    public function setSynopsis(?string $synopsis): self
+    {
+        $this->synopsis = $synopsis;
+
+        return $this;
+    }
+
+    public function getGenre(): ?string
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(string $genre): self
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    public function getPoster(): ?string
+    {
+        return $this->poster;
+    }
+
+    public function setPoster(?string $poster): self
+    {
+        $this->poster = $poster;
+
+        return $this;
+    }
+
+    public function getCensure(): ?string
+    {
+        return $this->censure;
+    }
+
+    public function setCensure(string $censure): self
+    {
+        $this->censure = $censure;
+
+        return $this;
+    }
+}
